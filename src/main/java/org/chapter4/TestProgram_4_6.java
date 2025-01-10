96ed39f5b34ceed81ba73c750af017f8c6dc0104
MU <mericajam@gmail.com>
2025-01-10 13:37:35 -0500

    Chapter 3 Exercises & Project

---
+++ src/main/java/org/chapter4/TestProgram_4_6.java
@@ -0,0 +1,27 @@
+package org.chapter4;
+
+public class TestProgram_4_6 {
+
+    public static void main(String[] args) {
+
+
+        String val = "22";
+        int x = 10;
+        x++;
+
+        {
+            int y = 2;
+            y++;
+            x++;
+        }
+
+        x++;
+       // y++;  out of scope variable
+
+
+
+
+
+
+    }
+}
